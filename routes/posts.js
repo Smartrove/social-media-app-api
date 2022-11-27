1ead96325e43de10ac4ed5b109f163366a2a06ed
Lateef Wasiu Olalekan <olaokunolalekan@gmail.com>
2022-11-27 17:48:21 +0100

    updating...

--- routes/posts.js
+++ routes/posts.js
@@ -15,16 +15,76 @@ router.post("/", async (req, res) => {
 
 //update post
 router.put("/:id", async (req, res) => {
-  const post = await Posts.findById(req.params.id);
-  if (post.userId === req.body.userId) {
-    try {
-      //
-    } catch (err) {
-      res.status(500).json(err);
+  try {
+    const post = await Posts.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      const updatedPost = await Posts.updateOne({ $set: req.body });
+      res.status(200).send("post updated successfully");
+    } else {
+      res.status(403).json("Post cannot be updated");
     }
-  } else {
-    res.status(403).json("Post cannot be updated");
+    //
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
+//delete post
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Posts.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      const updatedPost = await Posts.deleteOne();
+      res.status(200).send("post deleted successfully");
+    } else {
+      res.status(403).json("Post cannot be deleted");
+    }
+    //
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//like or unlike a post
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Posts.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).send("You liked this post");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).send("You unlike this post");
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+//get a post
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Posts.findById(req.params.id);
+    res.status(200).send(post);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+//get timeline posts
+router.get("/timeline/posts", async (req, res) => {
+  try {
+    const currentUser = await Posts.findById(req.body.userId);
+    const userPosts = await Posts.find({ userId: currentUser._id });
+    const friendsPosts = await Promise.all(
+      currentUser &&
+        currentUser.following.map((friendId) => {
+          return Posts.find({ userId: friendId });
+        })
+    );
+    res.json(userPosts.concat(...friendsPosts));
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 module.exports = router;
